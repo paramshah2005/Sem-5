cb4557f2107bc232a157cc90281dda78e348a030
Param Shah <99969531+paramshah2005@users.noreply.github.com>
2026-09-14 22:24:06 +0530

    Shorten Lab 8 CUDA programs

--- PCAPL/Lab 8/q4.cu
+++ PCAPL/Lab 8/q4.cu
@@ -5,16 +5,14 @@
 #define MAX_WORDS 100
 #define WORD_LEN 50
 
-__global__ void countWord(char *words, char *target, unsigned int *count,
-                           int numWords, int wordLen)
+__global__ void countWord(char *words, char *target, unsigned int *count, int numWords, int wordLen)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (i < numWords)
     {
         char *word = words + i * wordLen;
-        int j = 0;
-        int match = 1;
+        int j = 0, match = 1;
 
         while (word[j] != '\0' || target[j] != '\0')
         {
@@ -27,18 +25,17 @@ __global__ void countWord(char *words, char *target, unsigned int *count,
         }
 
         if (match)
+        {
             atomicAdd(count, 1);
+        }
     }
 }
 
 int main()
 {
-    char sentence[1000];
-    char target[WORD_LEN];
-    char words[MAX_WORDS][WORD_LEN];
+    char sentence[1000], target[WORD_LEN], words[MAX_WORDS][WORD_LEN];
     int numWords = 0;
     unsigned int count = 0;
-
     char *d_words, *d_target;
     unsigned int *d_count;
 
@@ -52,24 +49,20 @@ int main()
     char *token = strtok(sentence, " ");
     while (token != NULL && numWords < MAX_WORDS)
     {
-        strcpy(words[numWords], token);
-        numWords++;
+        strcpy(words[numWords++], token);
         token = strtok(NULL, " ");
     }
 
     cudaMalloc((void **)&d_words, MAX_WORDS * WORD_LEN * sizeof(char));
     cudaMalloc((void **)&d_target, WORD_LEN * sizeof(char));
     cudaMalloc((void **)&d_count, sizeof(unsigned int));
-
     cudaMemcpy(d_words, words, numWords * WORD_LEN * sizeof(char), cudaMemcpyHostToDevice);
     cudaMemcpy(d_target, target, WORD_LEN * sizeof(char), cudaMemcpyHostToDevice);
     cudaMemcpy(d_count, &count, sizeof(unsigned int), cudaMemcpyHostToDevice);
 
-    int threads = 256;
+    const int threads = 256;
     int blocks = (numWords + threads - 1) / threads;
-
     countWord<<<blocks, threads>>>(d_words, d_target, d_count, numWords, WORD_LEN);
-
     cudaMemcpy(&count, d_count, sizeof(unsigned int), cudaMemcpyDeviceToHost);
 
     printf("Word \"%s\" occurs %u times\n", target, count);
@@ -77,6 +70,5 @@ int main()
     cudaFree(d_words);
     cudaFree(d_target);
     cudaFree(d_count);
-
     return 0;
 }
