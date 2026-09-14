217e4b0a490df5845f49f33c61a727dfbddb7f94
Param Shah <99969531+paramshah2005@users.noreply.github.com>
2026-09-14 22:23:57 +0530

    Shorten Lab 8 CUDA programs

--- PCAPL/Lab 8/q3.cu
+++ PCAPL/Lab 8/q3.cu
@@ -11,7 +11,9 @@ __global__ void rankSort(int *a, int *b, int n)
         for (int j = 0; j < n; j++)
         {
             if (a[j] < a[i] || (a[j] == a[i] && j < i))
+            {
                 rank++;
+            }
         }
         b[rank] = a[i];
     }
@@ -19,42 +21,39 @@ __global__ void rankSort(int *a, int *b, int n)
 
 int main()
 {
-    int n, i;
-    int *a, *b;
-    int *d_a, *d_b;
+    int n;
+    int *a, *b, *d_a, *d_b;
 
     printf("Enter N: ");
     scanf("%d", &n);
-
     a = (int *)malloc(n * sizeof(int));
     b = (int *)malloc(n * sizeof(int));
 
     printf("Enter %d elements:\n", n);
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &a[i]);
+    }
 
     cudaMalloc((void **)&d_a, n * sizeof(int));
     cudaMalloc((void **)&d_b, n * sizeof(int));
-
     cudaMemcpy(d_a, a, n * sizeof(int), cudaMemcpyHostToDevice);
 
-    int threads = 256;
+    const int threads = 256;
     int blocks = (n + threads - 1) / threads;
-
     rankSort<<<blocks, threads>>>(d_a, d_b, n);
-
     cudaMemcpy(b, d_b, n * sizeof(int), cudaMemcpyDeviceToHost);
 
     printf("Sorted array:\n");
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", b[i]);
+    }
     printf("\n");
 
     cudaFree(d_a);
     cudaFree(d_b);
-
     free(a);
     free(b);
-
     return 0;
 }
